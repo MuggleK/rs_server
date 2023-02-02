fe741699a4bbe60f034ec4963ea4a0da6f062c1d
changchuan.pei <changchuan.pei@qizhidao.com>
2023-02-02 15:27:07 +0800

    update proxy_url

--- utils/proxy.py
+++ utils/proxy.py
@@ -10,7 +10,7 @@ from loguru import logger
 
 def get_proxies():
     try:
-        proxy = requests.get("http://127.0.0.1:5555/random").text.strip()
+        proxy = requests.get("http://192.168.4.155:5555/random").text.strip()   # proxy_url自定义
         return {'http': 'http://' + proxy, 'https': 'http://' + proxy}
     except:
         logger.error("获取代理失败")
